792893bc2a626c3f4a56b3bb311b7323687b9877
goranaviani <goranaviani@gmail.com>
2020-01-26 14:38:20 +0100

    print full launch info

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ print(request.status_code)
 for x in request.json()["launches"]:
     print("---------------")
     print(x)
-
+    moreInfoURL = "https://launchlibrary.net/1.3/launch/" + str(x['id'])
+    moreInfo = requests.get(moreInfoURL)
+    print(moreInfo.json())
+    print(moreInfo.status_code)
 
