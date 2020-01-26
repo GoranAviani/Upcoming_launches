7ded2b997a7dabce4fc7698f6314e5183fdf298f
goranaviani <goranaviani@gmail.com>
2020-01-26 14:26:52 +0100

    print launches

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 
 request = requests.get('https://launchlibrary.net/1.3/launch')
-print(request.text)
+print(request)
 print(request.status_code)
 
-for x in request:
+for x in request.json()["launches"]:
+    print("---------------")
     print(x)
 
 
